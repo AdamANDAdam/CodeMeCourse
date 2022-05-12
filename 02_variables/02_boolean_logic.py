f222b44d90840ec751d98006468a87ca9823eeaa
Adam <asur0008@Adams-MBP.lan>
2022-05-13 02:10:02 +1000

    TT

---
+++ 02_variables/02_boolean_logic.py
@@ -0,0 +1,16 @@
+# task1
+
+print(23 + 3 == 15 + 12)
+
+# task2
+
+print(29 // 7 == 5)
+
+# task3
+
+print(27 % 8 == 3)
+
+# task4
+
+number = 5
+print(f'Is {number} an even number?', number % 2 == 0)
