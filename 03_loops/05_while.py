8ff83718e3ae2e9f9da9523e7380a79395dbbc9a
Adam <asur0008@Adams-MBP.lan>
2022-05-17 03:16:01 +1000

    FC

---
+++ 03_loops/05_while.py
@@ -0,0 +1,14 @@
+price = 15
+while (price > 10):
+    print(price, "$ - za drogo")
+    price = price - 2
+
+print("------------")
+F = 200
+while (F > 0):
+    print(5 / 9 * (F - 32))
+    F-=20
+print("------------")
+
+for i in range (0, 200, 20):
+    print(5 / 9 * (i - 32))
